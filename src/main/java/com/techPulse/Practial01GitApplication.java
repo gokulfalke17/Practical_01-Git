325f6b8ba6dc5b6eac3bdf6763cab9c78a3d03ff
Gokul Falke <145555165+gokulfalke17@users.noreply.github.com>
2025-12-16 15:47:03 +0530

    Feat :: For Practice git with collabration

---
+++ src/main/java/com/techPulse/Practial01GitApplication.java
@@ -0,0 +1,13 @@
+package com.techPulse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Practial01GitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Practial01GitApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/techPulse/model/Calculator.java
@@ -0,0 +1,10 @@
+package com.techPulse.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+public class Calculator {
+    private Integer a;
+    private Integer b;
+}
